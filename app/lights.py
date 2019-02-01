2eae6b66974a0cd652c6b4fd6b644b59fdcf5d8f
Rylan Sturm <sturm.rw@gmail.com>
2019-01-31 21:12:42 -0700

    pinout changed for lights.py for new light system

--- app/lights.py
+++ app/lights.py
@@ -4,7 +4,7 @@ from gpiozero import LED
 class Light:
     r = LED(26, initial_value=True)
     g = LED(25, initial_value=True)
-    # b = LED(4, initial_value=True)
+    b = LED(4, initial_value=True)
 
     @staticmethod
     def red(state=False):
@@ -16,14 +16,14 @@ class Light:
         """ controls the green light (pin 25). True == on """
         Light.g.off() if state else Light.g.on()
 
-    # @staticmethod
-    # def buzzer(state=False):
-    #     """ controls the buzzer (pin 4). True == on """
-    #     Light.b.off() if state else Light.b.on()
+    @staticmethod
+    def buzzer(state=False):
+        """ controls the buzzer (pin 4). True == on """
+        Light.b.off() if state else Light.b.on()
 
     @staticmethod
-    def set_all(red=False, green=False):  # , buzzer=False):
+    def set_all(red=False, green=False, buzzer=False):
         """ controls all three pins. True == on """
         Light.red(red)
         Light.green(green)
-        # Light.buzzer(buzzer)
+        Light.buzzer(buzzer)
