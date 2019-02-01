6e5c7070c9a25e2c5193b4f10216e0bfae8de227
Rylan Sturm <sturm.rw@gmail.com>
2019-01-31 21:07:10 -0700

    pinout changed for lights.py for new light system

--- app/lights.py
+++ app/lights.py
@@ -2,28 +2,28 @@ from gpiozero import LED
 
 
 class Light:
-    r = LED(3, initial_value=True)
-    g = LED(2, initial_value=True)
-    b = LED(4, initial_value=True)
+    r = LED(26, initial_value=True)
+    g = LED(25, initial_value=True)
+    # b = LED(4, initial_value=True)
 
     @staticmethod
     def red(state=False):
-        """ controls the red light (pin 3). True == on """
+        """ controls the red light (pin 26). True == on """
         Light.r.off() if state else Light.r.on()
 
     @staticmethod
     def green(state=False):
-        """ controls the green light (pin 2). True == on """
+        """ controls the green light (pin 25). True == on """
         Light.g.off() if state else Light.g.on()
 
-    @staticmethod
-    def buzzer(state=False):
-        """ controls the buzzer (pin 4). True == on """
-        Light.b.off() if state else Light.b.on()
+    # @staticmethod
+    # def buzzer(state=False):
+    #     """ controls the buzzer (pin 4). True == on """
+    #     Light.b.off() if state else Light.b.on()
 
     @staticmethod
-    def set_all(red=False, green=False, buzzer=False):
+    def set_all(red=False, green=False):  # , buzzer=False):
         """ controls all three pins. True == on """
         Light.red(red)
         Light.green(green)
-        Light.buzzer(buzzer)
+        # Light.buzzer(buzzer)
