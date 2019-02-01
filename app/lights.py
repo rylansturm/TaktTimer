923ade5ec5c8be6b5724a23ed627cd43814f9d01
Rylan Sturm <sturm.rw@gmail.com>
2019-01-31 21:43:18 -0700

    changed lighting logic

--- app/lights.py
+++ app/lights.py
@@ -2,24 +2,24 @@ from gpiozero import LED
 
 
 class Light:
-    r = LED(25, initial_value=True)
-    g = LED(26, initial_value=True)
+    r = LED(26, initial_value=True)
+    g = LED(25, initial_value=True)
     b = LED(4, initial_value=True)
 
     @staticmethod
     def red(state=False):
-        """ controls the red light (pin 25). True == on """
-        Light.r.on() if state else Light.r.on()
+        """ controls the red light (pin 26). True == on """
+        Light.r.off() if not state else Light.r.on()
 
     @staticmethod
     def green(state=False):
-        """ controls the green light (pin 26). True == on """
-        Light.g.on() if state else Light.g.on()
+        """ controls the green light (pin 25). True == on """
+        Light.g.off() if not state else Light.g.on()
 
     @staticmethod
     def buzzer(state=False):
         """ controls the buzzer (pin 4). True == on """
-        Light.b.on() if state else Light.b.on()
+        Light.b.off() if not state else Light.b.on()
 
     @staticmethod
     def set_all(red=False, green=False, buzzer=False):
